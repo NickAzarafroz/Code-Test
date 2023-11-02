6d3a48fcf24f3cc91394d547d91d09fecb09f6a0
Nicckx <71626472+NickAzarafroz@users.noreply.github.com>
2023-11-02 01:22:19 +0100

    Added Magnet Item
    + Added a Magne Item which can be collected by the Player. If the Player collects the Magnet, it will appear above his head. While the Player has the Magnet equipped, all coins that are close to the Player will automatically move to him. The Magnet destroys itself after a certain amount of time.

---
+++ PajamaLlama-CodeTest/Assets/Nick_Azarafroz/Magnet.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");    
+    }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            transform.SetParent(collision.gameObject.transform, true);
+
+            transform.position = new Vector3(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y
+               + 0.7f, collision.gameObject.transform.position.z);
+
+            GetComponent<CircleCollider2D>().radius = 10f;
+            Destroy(gameObject, 12f);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Token")) 
+        {
+            Transform coinTransform = collision.transform;
+            Vector3 playerPosition = player.transform.position;
+
+            coinTransform.position = Vector3.Lerp(coinTransform.position, playerPosition, 5f * Time.deltaTime);
+        }
+    }
+}
